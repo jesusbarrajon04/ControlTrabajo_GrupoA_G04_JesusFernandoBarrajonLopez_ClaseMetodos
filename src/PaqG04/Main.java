453ee614b0de489db3322c9e6d82363e754961f1
Jesús Barrajón <JesusFernando.Barrajon@alu.uclm.es>
2023-05-10 22:51:26 +0200

    Prueba para el commit inicial

--- src/PaqG04/Main.java
+++ src/PaqG04/Main.java
@@ -2,6 +2,7 @@ package PaqG04;
 
 public class Main {
     public static void main(String[] args) {
+        // PRUEBA PARA EL COMMIT INICIAL
         Puerto puerto=new Puerto();             // CREACION DE PUERTO
     }
 }
